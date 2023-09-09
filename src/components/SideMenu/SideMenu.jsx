4219db7173092acf06f7e69d4bef47b716ca83d1
clf94-dev <clf94@hotmail.es>
2023-09-09 17:27:08 +0200

    - add router links to sideMenu

--- src/components/SideMenu/SideMenu.jsx
+++ src/components/SideMenu/SideMenu.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Outlet, Link } from 'react-router-dom';
 import { /* Breadcrumb, */ Layout, Menu } from 'antd';
 const { /* Header, Content, Footer, */ Sider } = Layout;
 import {
@@ -11,26 +12,28 @@ import {
 
 export function SideMenu() {
     const [collapsed, setCollapsed] = useState(false);
-    function getItem(label, key, icon, children) {
-        return {
-          key,
-          icon,
-          children,
-          label,
-        };
-      }
-    const items = [
-        getItem('Home', '1', <PieChartOutlined />),
-        getItem('First Exercise', '2', <DesktopOutlined />),
-        getItem('Second Exercise', 'sub2', <TeamOutlined />) 
-      ];
+    const [current, setCurrent] = useState('home');
+    const onClick = (e) => {
+    setCurrent(e.key);
+  };
       
     return (
         <>
             <Layout hasSider>
             <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)} style={{height: "100vh"}}>
                 <div className="demo-logo-vertical" />
-                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+                <Menu onClick={onClick} selectedKeys={[current]} mode="inline" theme='dark'>
+                    <Menu.Item key="home" icon= {<DesktopOutlined />}>
+                     <Link to="/">Home</Link>
+                    </Menu.Item>
+                    <Menu.Item key="first" icon= {<TeamOutlined />}>
+                      <Link to="/firstexercise">First Exercise</Link>
+                    </Menu.Item>
+                    <Menu.Item key="second" icon= {<PieChartOutlined />}>
+                      <Link to="/secondexercise">Second Exercise</Link>
+                    </Menu.Item>
+                </Menu>
+                <Outlet/>
             </Sider>
             </Layout>
       

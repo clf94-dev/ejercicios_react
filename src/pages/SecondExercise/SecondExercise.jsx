72190a588e1a9541961a5e63b4253fdb3d2104b2
clf94-dev <clf94@hotmail.es>
2023-09-10 08:34:36 +0200

    - add base card element

--- src/pages/SecondExercise/SecondExercise.jsx
+++ src/pages/SecondExercise/SecondExercise.jsx
@@ -1,9 +1,23 @@
 import { useEffect } from "react";
 import { Navbar } from "../../components/Navbar/Navbar";
 import { useStore } from "../../store/dataSlice";
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+   /*  Tooltip, */
+    /* Legend, */
+    Brush,
+    AreaChart,
+    Area,
+    ResponsiveContainer,
+  } from 'recharts';
 
 export function SecondExercise() {  
     const historyData = useStore(state => state.historyData)
+    const historyCases = useStore(state => state.historyCasesData)
     const requestHistoryExercise2 = useStore(state => state.requestHistoryExercise2)
     useEffect(() => {
         if(!historyData) {
@@ -11,12 +25,85 @@ export function SecondExercise() {
         }
     }, [])
 
-    console.log({historyData})
+    console.log({historyData, historyCases})
     return (
-        <div className="w-full">
+        <div className="w-full bg-gray-200">
             <Navbar />
             <div className="h-screen flex flex-col justify-evenly items-center m-auto">
                 <h1>COVID History data in the US</h1>
+                <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
+                    <div className="flex flex-col w-full justify-center m-auto p-4 bg-white rounded">
+                        <h3>Cases</h3>
+                        <div className="flex flex-col p-4 ">
+                            <p>Total</p>
+                            <ResponsiveContainer width="100%" height={200}>
+                              <AreaChart
+                                width={500}
+                                height={200}
+                                data={historyCases}
+                                syncId="anyId"
+                                margin={{
+                                  top: 10,
+                                  right: 30,
+                                  left: 0,
+                                  bottom: 0,
+                                }}
+                              >
+                                <CartesianGrid strokeDasharray="3 3" />
+                                <XAxis dataKey="date" tick={{fontSize: 10}} />
+                                <YAxis tick={{fontSize: 10}} />
+                                {/* <Tooltip /> */}
+                                <Area type="monotone" dataKey="total" stroke="#8884d8" fill="#8884d8" />
+                              </AreaChart>
+                            </ResponsiveContainer>
+                            <p>Change from previous day</p>
+                            <ResponsiveContainer width="100%" height={200}>
+                              <AreaChart
+                                width={500}
+                                height={200}
+                                data={historyCases}
+                                syncId="anyId"
+                                margin={{
+                                  top: 10,
+                                  right: 30,
+                                  left: 0,
+                                  bottom: 0,
+                                }}
+                              >
+                                <CartesianGrid strokeDasharray="3 3" />
+                                <XAxis dataKey="date" tick={{fontSize: 10}} />
+                                <YAxis tick={{fontSize: 10}} />
+                                {/* <Tooltip /> */}
+                                <Area type="monotone" dataKey="change_from_prior_day" stroke="#82ca9d" fill="#82ca9d" />
+                              </AreaChart>
+                            </ResponsiveContainer>
+                            <p>7 day percentage change (%)</p>
+                            <ResponsiveContainer width="100%" height={200}>
+                              <LineChart
+                                width={500}
+                                height={200}
+                                data={historyCases}
+                                syncId="anyId"
+                                margin={{
+                                  top: 10,
+                                  right: 30,
+                                  left: 0,
+                                  bottom: 0,
+                                }}
+                              >
+                                <CartesianGrid strokeDasharray="3 3" />
+                                <XAxis dataKey="date" tick={{fontSize: 10}} />
+                                <YAxis tick={{fontSize: 10}} />
+                                {/* <Tooltip /> */}
+                                <Line type="monotone" dataKey="seven_day_change_percent" stroke="#82ca9d" fill="#82ca9d" />
+                                <Brush dataKey="date" travellerWidth={1}/>
+                              </LineChart>
+                            </ResponsiveContainer>
+                            </div>
+                    </div>
+                    <div className="flex-col sm:w-3/6 md:w-2/6 m-auto">Testing</div>
+                    <div className="flex-col sm:w-3/6 md:w-2/6 m-auto">Deaths</div>
+                </div>
             </div>
         </div>
     )
--- src/store/dataSlice.js
+++ src/store/dataSlice.js
@@ -12,6 +12,9 @@ export const useStore = create((set) => ({
     setAgifyData: (data) => set({ agifyData: data }),
 
     historyData: undefined,
+    historyTestData: undefined,
+    historyCasesData: undefined,
+    historyDeathData: undefined,
     requestDataExercise1: async (pathParams) => {
         const [
             genderizeData,
@@ -43,11 +46,47 @@ export const useStore = create((set) => ({
          
           
         ]);
+
+        let testsData = []
+        let casesData = []
+        let deathData = []
+
+        historyData.data.forEach((date) => {
+            const testItem = {
+                date: date.date,
+                total: date.testing.total.value,
+                population_percent: date.testing.total.calculated.population_percent, 
+                change_from_prior_day: date.testing.total.calculated.change_from_prior_day,
+                seven_day_change_percent: date.testing.total.calculated.seven_day_change_percent,
+            }
+            const caseItem = {
+                date: date.date,
+                total: date.cases.total.value,
+                population_percent: date.cases.total.calculated.population_percent, 
+                change_from_prior_day: date.cases.total.calculated.change_from_prior_day,
+                seven_day_change_percent: date.cases.total.calculated.seven_day_change_percent,
+            }
+            const deathItem = {
+                date: date.date,
+                total: date.outcomes.death.total.value,
+                population_percent: date.outcomes.death.total.calculated.population_percent, 
+                change_from_prior_day: date.outcomes.death.total.calculated.change_from_prior_day,
+                seven_day_change_percent: date.outcomes.death.total.calculated.seven_day_change_percent,
+            }
+            testsData.push(testItem)
+
+            casesData.push(caseItem)
+            deathData.push(deathItem)
+        })
+
         
     
     
         set({
             historyData,
+            historyTestData: testsData,
+            historyCasesData: casesData,
+            historyDeathData: deathData,
         });
       },
 }));

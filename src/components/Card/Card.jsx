d0d76f136a350feae9cc2a10dd27bf3a6f130a89
clf94-dev <clf94@hotmail.es>
2023-09-10 14:46:25 +0200

    - add age & gender cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,24 @@
 import PropTypes from 'prop-types';
 import Stadistics from '../Stadistics/Stadistics';
 
-function Card({title, data, color, secondExercise}) {
+function Card({title, data, color, secondExercise, type}) {
     return (
-        <div className="card">
-            <div className="flex flex-col w-full items-center justify-centerr m-auto p-4 bg-white rounded">
+        <div className="card ">
+            <div className="flex flex-col h-full w-full justify-center m-auto p-4 bg-white rounded">
                 {secondExercise ? <>
                     <h3 className='font-bold text-xl '>{title}</h3>
                     <Stadistics brush={title === "Testing"} data={data} color={color} />
-                </> : null}
+                </> : <div className='flex flex-col '>
+                    {/* <h3 className='font-bold text-xl '>{title}</h3> */}
+                    <div className='flex flex-row  justify-center text-center mb-[3px] '>
+                        <p className='text-lg  pt-[4px] '>Most Likely {title}:</p>
+                        <p className='text-3xl font-bold text-blue-700 ml-3 capitalize'>{data[type]}</p>
+                    </div>
+                    {data.probability ? <div className='flex flex-row  justify-center text-center '>
+                        <p className='text-lg text-gray-500'>Probability:</p>
+                        <p className='text-xl font-bold text-blue-700 ml-3'>{data.probability * 100}%</p>
+                    </div>: null}
+                </div>}
             </div>
         </div>
     );
@@ -18,6 +28,7 @@ function Card({title, data, color, secondExercise}) {
         data: PropTypes.object,
         color: PropTypes.string,
         secondExercise: PropTypes.bool,
+        type: PropTypes.string
     }
 
     export default Card;
--- src/pages/FirstExerciseResults/FirstExerciseResults.jsx
+++ src/pages/FirstExerciseResults/FirstExerciseResults.jsx
@@ -1,14 +1,37 @@
 import { useNavigate } from "react-router-dom";
 import { Navbar } from "../../components/Navbar/Navbar";
+import { useStore } from "../../store/dataSlice";
+import Card from "../../components/Card";
 
 export function FirstExerciseResults() {
     let navigate = useNavigate(); 
+    const nameValue = useStore((state) => state.nameValue);
+    const agifyData = useStore((state) => state.agifyData);
+    const genderizeData = useStore((state) => state.genderizeData);
+    const nationalizeData = useStore((state) => state.nationalizeData);
+    console.log({agifyData, genderizeData, nationalizeData, nameValue})
     return (
-        <div className="w-full flex-col">
+        <div className="w-full h-screen flex-col bg-gray-200">
             <Navbar />
-            <div className="p-[30px]">
-                <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={() => navigate('/firstexercise')}> Back</button>
-            </div>
+            <div className="p-[30px] flex flex-row">
+                <div className=""> 
+                    <button className=" bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={() => navigate('/firstexercise')}> Back</button>
+                </div>
+                <div className="w-full h-full items-center flex flex-row ml-5"> 
+                    <p className="text-2xl ">Results for:</p>
+                    <p className="text-2xl font-bold text-blue-700 ml-3">{nameValue}</p> 
+                </div>
+                </div>
+                <div className="w-full grid grid-cols-1 lg:grid-cols-2 gap-5 p-4  text-center">
+                    {genderizeData ? <Card title="Gender" data={genderizeData} type="gender" /> : null}
+                    {agifyData ? <Card title="Age" data={agifyData} type="age"/> : null}
+                    
+                </div>
+                <div className="w-full grid grid-cols-1  gap-5 p-4">
+                    {nationalizeData ? <Card title="Nationality" data={nationalizeData} type="nationality"/> : null}
+            
+                </div>
+              
         </div>
     )
 }
